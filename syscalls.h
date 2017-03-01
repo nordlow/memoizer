8b916956b4e5fda1d51748cdfcbc9d740df3d891
Per Nordlöw <per.nordlow@gmail.com>
2017-03-01 21:15:13 +0100

    Fix

--- syscalls.h
+++ syscalls.h
@@ -1,11 +1,14 @@
 #define SYSCALL_MAXARGS 6
-enum argtype {
+
+enum argtype
+  {
     ARG_INT,
     ARG_PTR,
     ARG_STR
 };
 
-struct syscall_entry {
+struct syscall_entry
+{
     const char *name;
     int nargs;
     enum argtype args[SYSCALL_MAXARGS];
