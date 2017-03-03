f1ea00a89f0db32d8b0c274c349a8c8d968b078c
Per Nordlöw <per.nordlow@gmail.com>
2017-03-03 19:25:33 +0100

    Fix

--- tests/cxx-compilation/main.cpp
+++ tests/cxx-compilation/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <cassert>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 using std::cout;
 using std::endl;
@@ -8,6 +13,12 @@ using std::dec;
 
 int main(int argc, const char * argv[], const char * envp[])
 {
-    std::cout << "Hello world!" << endl;
+    chdir("/etc");
+
+    struct stat st;
+    assert(stat("passwd", &st) == 0);
+
+    std::cout << "All ok!" << endl;
+
     return 0;
 }
