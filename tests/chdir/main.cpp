975b07b7193351441f633e11796be21e22404f45
Per Nordlöw <per.nordlow@gmail.com>
2017-03-07 15:45:11 +0100

    Fix

--- tests/chdir/main.cpp
+++ tests/chdir/main.cpp
@@ -9,25 +9,33 @@
 
 int main(int argc, const char * argv[], const char * envp[])
 {
-    const int new_fd = open("test_output", O_WRONLY | O_CREAT | O_TRUNC, 0777);
-    assert(new_fd);
-    assert(close(new_fd) >= 0);
+    {
+        const int new_fd = open("test_output", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        assert(new_fd);
+        assert(close(new_fd) >= 0);
+    }
 
-    const int main_fd = open("main.cpp", O_RDONLY);
-    assert(main_fd);
-    assert(close(main_fd) >= 0);
+    {
+        const int main_fd = open("main.cpp", O_RDONLY);
+        assert(main_fd);
+        assert(close(main_fd) >= 0);
+    }
 
     chdir("/etc");
 
-    struct stat st;
-    assert(stat("passwd", &st) == 0);
-    assert(st.st_mtime);
-    assert(st.st_ctime);
-    assert(st.st_atime);
+    {
+        struct stat st;
+        assert(stat("passwd", &st) == 0);
+        assert(st.st_mtime);
+        assert(st.st_ctime);
+        assert(st.st_atime);
+    }
 
-    const int passwd_fd = open("passwd", O_RDONLY);
-    assert(passwd_fd);
-    assert(close(passwd_fd) >= 0);
+    {
+        const int passwd_fd = open("passwd", O_RDONLY);
+        assert(passwd_fd);
+        assert(close(passwd_fd) >= 0);
+    }
 
     printf("All ok!\n");
 
