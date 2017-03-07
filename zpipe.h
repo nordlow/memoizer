343d57358a45a937ef3016ae3231a0cf8589ce2e
Per Nordlöw <per.nordlow@gmail.com>
2017-03-07 20:15:03 +0100

    Fix

--- zpipe.h
+++ zpipe.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* Compress from file source to file dest until EOF on source.
+/* Compress from file `source` to file `dest` until EOF on `source`.
 
    Returns: Z_OK on success, Z_MEM_ERROR if memory could not be allocated for
    processing, Z_STREAM_ERROR if an invalid compression level is supplied,
@@ -9,7 +9,7 @@
    files. */
 int z_compress(FILE* source, FILE* dest, int level);
 
-/** Decompress from file source to file dest until stream ends or EOF.
+/** Decompress from file `source` to file `dest` until stream ends or EOF.
 
    Returns: Z_OK on success, Z_MEM_ERROR if memory could not be allocated for
    processing, Z_DATA_ERROR if the deflate data is invalid or incomplete,
