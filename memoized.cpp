f2b96ba2a81b16998aa9f748f14ea1c47bb9e35a
Per Nordlöw <per.nordlow@gmail.com>
2017-03-07 10:36:19 +0100

    Fix

--- memoized.cpp
+++ memoized.cpp
@@ -1,7 +1,8 @@
 // TODO add check for non-existing PROGRAM in call: ./memoized PROGRAM, for
 // instance when `foo` is given when `./foo` should be given
 
-// TODO add flag --env= to limit environment of called process
+// TODO add flag --no-env to skip environemnt
+// TODO add flag --set-env= to set environment
 
 #include <sys/ptrace.h>
 #include <bits/types.h>
